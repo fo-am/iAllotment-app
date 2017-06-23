ae5ab38488899ebcd781f92fde8803a2fb11cd79
Aidan Weatherill <Aidan@fo.am>
2017-06-23 14:56:47 +0100

    align the icon to the centre

--- compost-app/App.js
+++ compost-app/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
 
         <Text style={styles.header}>
           <Text>{'The Allotment Lab\nPrototype'}</Text>
-        
+
         </Text>
         <Text style={styles.normal}>This is a prototype designed to
             demonstrate ideas for the Allotment Lab</Text>
@@ -29,15 +29,21 @@ export default class App extends Component {
             of the <Text style={styles.fakeLink}>SWARM Knowledge Hub</Text>, a Rural Develpment Programme for England
              (RDPE) initative managed by <Text style={styles.fakeLink}>Duchy College Rural Business School</Text>,
              in partnership with Rothamsted Research North Wyke.</Text>
-        <Image style={{ alignItems: 'center', }}
+        <View style={{ alignItems: 'center' }}><Image
           source={require('./assets/images/logo.png')}
-        />
+        /></View>
         <Button
           onPress={this._onPressLearnMore}
           title="Enter the Allotment Lab"
           color='rgb(224, 190, 54)'
           accessibilityLabel="Enter the Allotment Lab"
         />
+         <Text style={styles.normal}>The Allotment Lab offers informaion
+            for guidance purposes only and is not intended to amount to
+             professional advice or opinion. FoAM Kernow, Duchy College, 
+             and Rothamstead Reasearch North Wyke cannot be held responsible 
+             for any losses or damage resultiong from the use of information 
+             provided by this app.</Text>
       </ScrollView>
     );
   }
