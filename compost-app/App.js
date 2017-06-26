ade8e88276c15878397f66286eafa0d2c6ec7269
Aidan Weatherill <Aidan@fo.am>
2017-06-26 15:23:26 +0100

    Change to react-native-router-flux for navigation. add a splash and a main page.

--- compost-app/App.js
+++ compost-app/App.js
@@ -1,83 +1,18 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
-import { Font, AppLoading, WebBrowser } from 'expo'
+import React, { Component } from "react";
+import { Router, Scene } from "react-native-router-flux";
 
+import Splash from "./Splash";
+import Main from "./Main";
 
 export default class App extends Component {
-  state = {
-    isReady: false,
-  };
-
-  componentWillMount() {
-    this._cacheResourcesAsync();
-  }
-
   render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
     return (
-      <ScrollView style={styles.background}>
-
-        <Text style={styles.header}>
-          <Text>{'The Allotment Lab\nPrototype'}</Text>
-
-        </Text>
-        <Text style={styles.normal}>This is a prototype designed to
-            demonstrate ideas for the Allotment Lab</Text>
-        <Text style={styles.normal}>Developed by <Text style={styles.fakeLink}>FoAM Kernow</Text> on behalf
-            of the <Text style={styles.fakeLink}>SWARM Knowledge Hub</Text>, a Rural Develpment Programme for England
-             (RDPE) initative managed by <Text style={styles.fakeLink}>Duchy College Rural Business School</Text>,
-             in partnership with Rothamsted Research North Wyke.</Text>
-        <View style={{ alignItems: 'center' }}><Image
-          source={require('./assets/images/logo.png')}
-        /></View>
-        <Button
-          onPress={this._onPressLearnMore}
-          title="Enter the Allotment Lab"
-          color='rgb(224, 190, 54)'
-          accessibilityLabel="Enter the Allotment Lab"
-        />
-         <Text style={styles.normal}>The Allotment Lab offers informaion
-            for guidance purposes only and is not intended to amount to
-             professional advice or opinion. FoAM Kernow, Duchy College, 
-             and Rothamstead Reasearch North Wyke cannot be held responsible 
-             for any losses or damage resultiong from the use of information 
-             provided by this app.</Text>
-      </ScrollView>
+      <Router>
+        <Scene key="root">
+          <Scene key="splash" component={Splash} initial={true} />
+          <Scene key="main" component={Main} title="PageTwo" />
+        </Scene>
+      </Router>
     );
   }
-  _onPressLearnMore() { }
-
-  async _cacheResourcesAsync() {
-    await Font.loadAsync({
-      'aria-madurai': require('./assets/fonts/ArimaMadurai-Regular.ttf'),
-    });
-
-    this.setState({ isReady: true });
-  }
 }
-
-const styles = StyleSheet.create({
-  header: {
-    textAlign: 'center',
-    fontFamily: 'aria-madurai',
-    fontSize: 30,
-  },
-  normal: {
-    textAlign: 'center',
-    fontFamily: 'aria-madurai',
-    fontSize: 20,
-  },
-  fakeLink: {
-    textDecorationLine: 'underline',
-    color: 'rgb(0, 170, 0)',
-    textAlign: 'center',
-    fontFamily: 'aria-madurai',
-    fontSize: 20,
-  },
-  background: {
-
-    backgroundColor: 'rgb(243, 247, 235)',
-  }
-});
