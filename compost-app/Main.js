a20ffb5f1c5fa705b1298ef389edf9f9f9c28607
Aidan Weatherill <Aidan@fo.am>
2017-06-28 16:23:37 +0100

    Make the image show and scale through rotation

--- compost-app/Main.js
+++ compost-app/Main.js
@@ -1,13 +1,66 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Actions } from "react-native-router-flux";
+import React, {Component} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Button,
+  Dimensions
+} from "react-native";
+import resolveAssetSource from "resolveAssetSource";
+import {Actions} from "react-native-router-flux";
 
 export default class Main extends Component {
+  measureView(event) {
+    this.setState({
+      width: event.nativeEvent.layout.width,
+      height: event.nativeEvent.layout.width
+    });
+  }
+  componentWillMount() {
+    let source = resolveAssetSource(require("./assets/images/map.png"));
+    this.setState({ratio: source.height / source.width});
+  }
   render() {
     return (
-      <View style={{ margin: 128 }}>
-        <Text onPress={Actions.splash}>This is PageTwo!</Text>
-      </View>
+      <ScrollView
+        style={styles.background}
+        onLayout={event => this.measureView(event)}
+      >
+        <Text style={styles.header}>
+          Becky's allotment
+        </Text>
+
+        <Image
+          style={{
+            width: this.state.width,
+            height: this.state.height * this.state.ratio
+          }}
+          resizeMode="contain"
+          source={require("./assets/images/map.png")}
+        />
+
+        <Text>
+          {" "}
+          Here is some exciting content about mud.
+        </Text>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: "stretch"
+  },
+  header: {
+    textAlign: "center",
+    fontFamily: "aria-madurai",
+    fontSize: 30
+  },
+  background: {
+    paddingTop: 40,
+    backgroundColor: "rgb(243, 247, 235)"
+  }
+});
