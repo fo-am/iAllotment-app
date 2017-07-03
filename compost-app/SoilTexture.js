091c77dd45560ca14960a25e719b176e3d1c69b7
Aidan Weatherill <Aidan@fo.am>
2017-07-04 00:44:50 +0100

    add showif magic demo showing different sections

--- compost-app/SoilTexture.js
+++ compost-app/SoilTexture.js
@@ -3,20 +3,46 @@ import {StyleSheet, Text, View, ScrollView, Image, Button} from "react-native";
 import {Font, AppLoading, WebBrowser} from "expo";
 import {Actions} from "react-native-router-flux";
 import styles from "./Styles";
+import renderif from "./RenderIf";
 
 export default class SoilTexture extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {page: "one"};
+  }
   render() {
     return (
-      <ScrollView style={styles.background}>
-        <Text style={styles.header}>
-          <Text>{"The Allotment Lab\nPrototype"}</Text>
-        </Text>
-        <Text style={styles.normal}>
-          This is a prototype designed to
-          demonstrate ideas for the Allotment Lab
-        </Text>
+      <View>
+        {renderif(this.state.page === "one")(
+          <ScrollView style={styles.background}>
 
-      </ScrollView>
+            <Text style={styles.header}>
+              <Text>{"Page one"}</Text>
+            </Text>
+            <Text style={styles.normal}>
+              This is page one
+            </Text>
+            <Button
+              title="This is a button to two"
+              onPress={() => this.setState({page: "two"})}
+            />
+          </ScrollView>
+        )}
+        {renderif(this.state.page === "two")(
+          <ScrollView style={styles.background}>
+            <Text style={styles.header}>
+              <Text>{"Page two"}</Text>
+            </Text>
+            <Text style={styles.normal}>
+              This is page two
+            </Text>
+            <Button
+              title="This is a button to one"
+              onPress={() => this.setState({page: "one"})}
+            />
+          </ScrollView>
+        )}
+      </View>
     );
   }
 }
